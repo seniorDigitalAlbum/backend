6e044c42650be793784b08bc8e7c83902cd3452b
chaehyunkIM <chaehhyun0803@pusan.ac.kr>
2025-05-17 16:28:48 +0900

    initial commit

---
+++ src/test/java/com/chimaenono/dearmind/DearmindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chimaenono.dearmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DearmindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

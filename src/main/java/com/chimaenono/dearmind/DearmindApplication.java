a364a95fd2a1ef967521b62582e882db6968e519
chaehyunkIM <chaehhyun0803@pusan.ac.kr>
2025-05-17 16:28:48 +0900

    initial commit

---
+++ src/main/java/com/chimaenono/dearmind/DearmindApplication.java
@@ -0,0 +1,13 @@
+package com.chimaenono.dearmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DearmindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DearmindApplication.class, args);
+	}
+
+}
